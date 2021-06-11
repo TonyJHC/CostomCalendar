27bd606848441c85a2d7d9e588f01e1b0636d08b
kimdavin <80314152+kimdavin@users.noreply.github.com>
2021-06-11 19:12:22 +0900

    final

--- app/src/main/java/com/hansung/android/homework3_final_test/MainActivity2.java
+++ app/src/main/java/com/hansung/android/homework3_final_test/MainActivity2.java
@@ -13,6 +13,10 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+<<<<<<< Updated upstream
+=======
+import android.util.Log;
+>>>>>>> Stashed changes
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -50,7 +54,15 @@ import java.util.Locale;
 public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallback {
 
     private FusedLocationProviderClient mFusedLocationClient; //통합 위치 정보 제공자 (Fused Location Provider) 클라이언트 객체를 얻어온다.
+<<<<<<< Updated upstream
     private GoogleMap googleMap;
+=======
+    private GoogleMap mGoogleMap = null;
+    private Geocoder geocoder; //주소를 위도,경도로 변경해주는 Geocoding
+    private Button btn;
+    private EditText edt;
+
+>>>>>>> Stashed changes
 
     private int mYear;
     private int mMonth;
@@ -64,10 +76,14 @@ public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallba
         setContentView(R.layout.activity_main2);
         setTitle("Calender App");
 
+<<<<<<< Updated upstream
         //https://developer88.tistory.com/39 timepicker 설정하는법
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+=======
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+>>>>>>> Stashed changes
         getLastLocation();
 
         //MainActivity에서 날짜를 넘겨받음
@@ -83,6 +99,10 @@ public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallba
         text.setText(mYear + "년 " + mMonth + "월 " + mDay + "일 " + mHour + "시");
 
         //타임피커를 선택한 시간으로 설정
+<<<<<<< Updated upstream
+=======
+        //출처: https://developer88.tistory.com/39 timepicker 설정하는법
+>>>>>>> Stashed changes
         TimePicker tp = findViewById(R.id.time_picker1);
         tp.setCurrentHour(mHour);
         tp.setCurrentMinute(mMinute);
@@ -90,11 +110,59 @@ public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallba
         tp = findViewById(R.id.time_picker2);
         tp.setCurrentHour(mHour+1);
         tp.setCurrentMinute(mMinute);
+<<<<<<< Updated upstream
+=======
+
+
+        //위치정보를 얻기위한 권한을 획득, AndroidManifest.xml
+        //INTERNET : 구글서버에 접근하기위해서 필요함
+
+        btn = (Button)findViewById(R.id.find);
+        edt = (EditText)findViewById(R.id.input);
+
+        geocoder = new Geocoder(this);
+
+        //주소를 위도,경도로 변경해주는 Geocoding을 이용하여 변경된 위도,경도를 지도에 띄움
+        //출처: https://bitsoul.tistory.com/135
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //주소를 입력후 찾기버튼 클릭시 해당 위도,경도를 지도에 띄움
+                List<Address> list = null;
+
+                String str = edt.getText().toString(); 
+                try {
+                    list = geocoder.getFromLocationName
+                            (str, //지역 이름
+                                    10); //읽을 개수
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("test","입출력 오류 - 서버에서 주소변환시 에러발생");
+                }
+
+                if (list != null) {
+                    Address addr = list.get(0);
+                    double lat = addr.getLatitude();
+                    double lon = addr.getLongitude();
+
+                    LatLng location2 = new LatLng(lat, lon);
+
+                    mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location2,15));
+                    mGoogleMap.addMarker(new MarkerOptions().position(location2));
+                }
+
+            }
+        });
+>>>>>>> Stashed changes
     }
 
     final int REQUEST_PERMISSIONS_FOR_LAST_KNOWN_LOCATION = 0;
     Location mLastLocation;
 
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
     private void getLastLocation() {
         // 1. 위치 접근에 필요한 권한 검사 및 요청
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
@@ -132,10 +200,18 @@ public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallba
     }
 
 
+<<<<<<< Updated upstream
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
 
+=======
+
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mGoogleMap = googleMap; //위에 moveCamera랑 겹쳐서 오류가 뜨는것을 방지
+>>>>>>> Stashed changes
         //LatLng hansung = new LatLng(37.5817891, 127.008175);
         if (mLastLocation == null)
             return;
@@ -144,7 +220,10 @@ public class MainActivity2 extends AppCompatActivity implements OnMapReadyCallba
         // move the camera
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15));
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     }
 
 }
